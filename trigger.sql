cac4a8a0d13003a90bcb109981331fd792561421
Suresh <vuppala.sureshbabu@cognizant.com>
2024-03-31 19:59:32 -0400

    updated db creds

--- trigger.sql
+++ trigger.sql
@@ -1,10 +1,5 @@
 USE hotel;
 
-
--- Set the global variable to allow trigger creation
-SET GLOBAL log_bin_trust_function_creators = 1;
-
--- Create the trigger
 DELIMITER //
 CREATE TRIGGER calculate_days_trigger
 BEFORE INSERT ON booking
