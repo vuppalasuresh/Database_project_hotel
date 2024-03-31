a6f9e219a688256ed69bf24b553ed75de3e06269
Suresh <vuppala.sureshbabu@cognizant.com>
2024-03-31 19:57:46 -0400

    updated db creds

--- trigger.sql
+++ trigger.sql
@@ -1,7 +1,5 @@
 USE hotel;
 
--- Set the session variable to allow trigger creation
-SET @@SESSION.SQL_LOG_BIN = 0;
 
 -- Set the global variable to allow trigger creation
 SET GLOBAL log_bin_trust_function_creators = 1;
